a3038c7989d8f94042ee19a3cd3e360e2db94a5e
126Nabil-M <113116225+126Nabil-M@users.noreply.github.com>
2024-02-21 12:36:37 -0500

    Update Gradebook.java

--- Gradebook.java
+++ Gradebook.java
@@ -1,2 +1,45 @@
+import java.util.ArrayList;
+
 public class Gradebook {
+    private ArrayList<Student> Name = new ArrayList<>();
+    private ArrayList<Object> Grades = new ArrayList<>();
+    public Gradebook(){
+
+    }
+    public void createStudent(String name){
+        String student = name;
+    }
+    public void addAssignment(String assignment){
+        String project = assignment;
+    }
+    public void gradeAssignment(String assignment, String studentName, double grade){
+
+    }
+    public double getAverageGrade(String assignment){
+
+    }
+    public double getMinimumGrade(String assignment){
+
+    }
+    public double getMaximumGrade(String assignment){
+
+    }
+    public double getAverageOverall(){
+
+    }
+    public double getMaxOverall(){
+
+    }
+    public double getMinOverall(){
+
+    }
+   public double getStudentsGrade(){
+
+    }
+    public String toString(){
+
+    }
+    public Student getStudent(String studentName){
+
+    }
 }
